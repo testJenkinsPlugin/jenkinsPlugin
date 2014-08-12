b8b293a2f1d559cae7bc3084185a4e2fe0839df2
amatviychuk@amcbridge.com.ua <amatviychuk@amcbridge.com.ua@7654da4f-31dd-439e-8b74-b8f99c475f98>
2014-08-12 10:25:45 +0000

    Bug 13547 - Retrieve and store static configuration data to Jenkins XML files (code review)

--- configurator/src/main/java/com/amcbridge/jenkins/plugins/configurator/BuildConfiguration.java
+++ configurator/src/main/java/com/amcbridge/jenkins/plugins/configurator/BuildConfiguration.java
@@ -1,28 +1,82 @@
 package com.amcbridge.jenkins.plugins.configurator;
 
+import hudson.XmlFile;
+import java.io.File;
+import java.io.IOException;
+import jenkins.model.Jenkins;
+import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 
 @XStreamAlias("buildConfiguration")
 public class BuildConfiguration {
-	
-    private String name;
+
+    private static final String CONFIG_FILE_NAME = "config.xml";
+    private static final String BUILDCONFIGURATOR_DIRECTORY_NAME = "BuildConfiguration";
+
+    static {Jenkins.XSTREAM.processAnnotations(BuildConfiguration.class);}
+
+    private String entityName;
+    private String creator;
 
     public BuildConfiguration(String name)
     {
-    	this.name = name;
+        this.entityName = name;
     }
-    
+
     public BuildConfiguration()
     {
-    	name = "";
+        entityName = "";
     }
-    
+
     public String getName()
     {
-        return name;
+        return entityName;
     }
+
     public void setName(String name) 
     {
-        this.name = name;
+        this.entityName = name;
     }
+
+    public String getCreator()
+    {
+        return creator;
+    }
+    
+    public void setCreator(String creat) 
+    {
+        this.creator = creat;
+    }
+
+    private static File getConfigFileFor(String id) 
+    {
+        return new File(new File(getRootDir(), id), CONFIG_FILE_NAME);
+    }
+
+    private static File getRootDir() 
+    {
+        return new File(Jenkins.getInstance().getRootDir(), BUILDCONFIGURATOR_DIRECTORY_NAME);
+    }
+
+    public void save() throws IOException 
+    {
+        XStream xs = new XStream();
+        xs.alias("BuildConfiguration",  BuildConfiguration.class);
+        XmlFile fileWriter = new XmlFile(Jenkins.XSTREAM, getConfigFileFor(BUILDCONFIGURATOR_DIRECTORY_NAME));
+        fileWriter.write(this); 
+    }
+
+    protected final XmlFile getConfigFile() 
+    {
+        return new XmlFile(Jenkins.XSTREAM, getConfigFileFor(BUILDCONFIGURATOR_DIRECTORY_NAME));
+    }
+
+    public void load() throws IOException
+    {
+        XmlFile config = getConfigFile();
+            if (config.exists()) 
+            {
+                config.unmarshal(this);
+            }
+     }
 }
--- configurator/src/main/java/com/amcbridge/jenkins/plugins/configurator/BuildConfigurator.java
+++ configurator/src/main/java/com/amcbridge/jenkins/plugins/configurator/BuildConfigurator.java
@@ -1,34 +1,23 @@
 package com.amcbridge.jenkins.plugins.configurator;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-
-import com.thoughtworks.xstream.XStream;
-
 import javax.servlet.ServletException;
-
-import jenkins.model.Jenkins;
 import net.sf.json.JSONObject;
-
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
-
 import hudson.Extension;
-import hudson.XmlFile;
 import hudson.model.RootAction;
 
 @Extension
 public final class BuildConfigurator implements RootAction
 {
-	 BuildConfiguration buildConfig = new BuildConfiguration();
-	 private String name = "BuildConfiguration";
-	 
-	 private static final String CONFIG_FILE_NAME = "config.xml";
-	 private static final String BuildConfigurator_DIRECTORY_NAME = "configurator";
-	 
-     static {Jenkins.XSTREAM.processAnnotations(BuildConfiguration.class);}
-     
+     BuildConfiguration buildConfig;
+
+     public BuildConfigurator()
+     {
+         buildConfig = new BuildConfiguration();
+     }
+
      public String getDisplayName() 
      {
           return "Build Configurator";
@@ -43,77 +32,39 @@ public final class BuildConfigurator implements RootAction
      {
          return "BuildConfigurator";
      }
-        
+
      public void doSave(final StaplerRequest request, final StaplerResponse response) throws ServletException, IOException
      { 
-     	 try
-     	 {
-     		 JSONObject formData = request.getSubmittedForm();
-     		 String TextBox1 = formData.getString("TextBox1");
-     		 buildConfig = new BuildConfiguration(TextBox1);
-     		 save();
-       	}
-       	finally
-       	{
+          try
+          {
+              JSONObject formData = request.getSubmittedForm();
+              String textName = formData.getString("TextName");
+              buildConfig = new BuildConfiguration(textName);
+              buildConfig.save();
+           }
+           finally
+           {
              response.sendRedirect("../BuildConfigurator");            
         }
      }
 
-     private static File getConfigFileFor(String id) 
-     {
-         return new File(new File(getRootDir(), id), CONFIG_FILE_NAME);
-     }
-     
-     private static File getRootDir() 
-     {
-         return new File(Jenkins.getInstance().getRootDir(), BuildConfigurator_DIRECTORY_NAME);
-     }
-     
-     private void save() throws IOException 
-     {
-		XStream xs = new XStream();
-		xs.alias("BuildConfiguration",  BuildConfiguration.class);
-		try 
-		{
-			XmlFile qewrty = new XmlFile(Jenkins.XSTREAM, getConfigFileFor(name));
-			qewrty.write(buildConfig); 
-        } 
-		catch (FileNotFoundException e1)
-		{
-            e1.printStackTrace();
-        }
-     }
-		
-     protected final XmlFile getConfigFile() 
-     {
-         return new XmlFile(Jenkins.XSTREAM, getConfigFileFor(name));
-     }
-     
-	private void load() throws IOException 
-	{
-		XmlFile config = getConfigFile();
-        try 
+    private void load() throws IOException
+    {
+        buildConfig.load();
+    }
+
+    public BuildConfiguration getBuildConfig() throws IOException 
+    {
+        try
         {
-            if (config.exists()) 
-            {
-                config.unmarshal(buildConfig);
+            if (buildConfig.getName() == "")
+            { 
+                load();
             }
-        } 
-        catch (IOException e) { }
-	 }
-		
-	public BuildConfiguration getPeople() throws IOException 
-	{
-		try
-		{
-			if (buildConfig.getName() == "")
-			{ 
-				load();
-			}
-		}
-		finally
-		{
-			return buildConfig;
-		}
-	}
+        }
+        finally
+        {
+            return buildConfig;
+        }
+    }
 }
