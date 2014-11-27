13a47a3f321b6a960433b43db8def1efeaf8e1a8
amatviychuk@amcbridge.com.ua <amatviychuk@amcbridge.com.ua@7654da4f-31dd-439e-8b74-b8f99c475f98>
2014-11-27 13:46:25 +0000

    Bug 15336 - Implement logging into Jenkins with TTS credentials

---
+++ ttsAuthorization/src/main/java/com/amcbridge/jenkins/plugins/TTS/TTSSecurityRealm.java
@@ -0,0 +1,76 @@
+package com.amcbridge.jenkins.plugins.TTS;
+
+import org.acegisecurity.Authentication;
+import org.acegisecurity.AuthenticationException;
+import org.acegisecurity.AuthenticationManager;
+import org.acegisecurity.BadCredentialsException;
+import org.acegisecurity.userdetails.UserDetails;
+import org.acegisecurity.userdetails.UserDetailsService;
+import org.acegisecurity.userdetails.UsernameNotFoundException;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.springframework.dao.DataAccessException;
+
+import hudson.Extension;
+import hudson.model.Descriptor;
+import hudson.model.Hudson;
+import hudson.security.SecurityRealm;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterConfig;
+
+public class TTSSecurityRealm extends SecurityRealm {
+
+	private static String LOGIN_EXCEPTION = "Unexpected authentication type: ";
+
+	@DataBoundConstructor
+	public TTSSecurityRealm()
+	{
+		super();
+	}
+
+	@Override
+	public boolean allowsSignup() {
+		return false;
+	}
+
+	@Override
+	public Filter createFilter(FilterConfig filterConfig) {
+		Filter defaultFilter = super.createFilter(filterConfig);
+		return new TTSServletFilter(defaultFilter);
+	}
+
+	@Override
+	public SecurityComponents createSecurityComponents() {
+		Hudson.getInstance().setDisableRememberMe(true);
+		return new SecurityComponents(
+				new AuthenticationManager() {
+					public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+						try {
+							if (TTSServiceConnection.checkUser(authentication.getName(), authentication.getCredentials().toString())) {
+								return authentication;
+							}
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+						throw new BadCredentialsException(LOGIN_EXCEPTION + authentication);
+					}
+				}, new UserDetailsService() {
+					public UserDetails loadUserByUsername(String username)
+							throws UsernameNotFoundException, DataAccessException {
+						return TTSSecurityRealm.this.loadUserByUsername(username);
+					}
+				}
+				);
+	}
+
+	@Extension
+	public static final class DescriptorImpl extends Descriptor<SecurityRealm> {
+
+		private static final String DISPLAY_NAME = "TTS";
+
+		@Override
+		public String getDisplayName() {
+			return DISPLAY_NAME;
+		}
+	}
+}
