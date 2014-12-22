c0bcf38b9be84ad1bd5ec12943ea8db5ac74ce33
klukyanenko@amcbridge.com <klukyanenko@amcbridge.com@7654da4f-31dd-439e-8b74-b8f99c475f98>
2014-12-22 10:30:31 +0000

    Bug 15773 -  Error message is incorrect

--- trunk/configurator/src/main/java/com/amcbridge/jenkins/plugins/vsc/CommitError.java
+++ trunk/configurator/src/main/java/com/amcbridge/jenkins/plugins/vsc/CommitError.java
@@ -2,7 +2,7 @@ package com.amcbridge.jenkins.plugins.vsc;
 
 public enum CommitError
 {
-	NONE_PROPERTY("Please set all information, in Jenkins manage, for exporting XML file to SVN repository."),
+	NONE_PROPERTY("Please set all information in Build automation management tool settings."),
 	FAIL("Cannot connect with SVN repository. Please check your settings in manage Jenkins.");
 
 	private String bmcValue;
