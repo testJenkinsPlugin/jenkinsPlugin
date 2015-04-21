b7fa40d9dd4255d28d298e7e870b7b83be0af2af
akarlechuk@amcbridge.com <akarlechuk@amcbridge.com@7654da4f-31dd-439e-8b74-b8f99c475f98>
2015-04-21 08:46:01 +0000

    Bug 18201 - Job's update failure at Jenkins versions higher than 1.563

--- trunk/configurator/src/main/java/com/amcbridge/jenkins/plugins/job/JobManagerGenerator.java
+++ trunk/configurator/src/main/java/com/amcbridge/jenkins/plugins/job/JobManagerGenerator.java
@@ -21,7 +21,9 @@ import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.sax.SAXSource;
 import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 
 import jenkins.model.Jenkins;
 
@@ -64,8 +66,9 @@ public class JobManagerGenerator {
 			DocumentBuilder docBuilder;
 			Document doc = null;
 			docBuilder = docFactory.newDocumentBuilder();
-			doc = docBuilder.parse(getJobXML(config));
-			Source streamSource = new DOMSource( doc );
+			//doc = docBuilder.parse(getJobXML(config));
+			//Source streamSource = new DOMSource( doc );
+			Source streamSource = new StreamSource(getJobXML(config));
 			item.updateByXml(streamSource);
 			item.save();
 		}
