7bf8717488fe209fd54e09fcdfa47838d911eb4a
rbilyi@amcbridge.biz <rbilyi@amcbridge.biz@7654da4f-31dd-439e-8b74-b8f99c475f98>
2015-10-06 05:16:42 +0000

    nobug remove Dev branch

--- branches/Dev/configurator/src/main/java/com/amcbridge/jenkins/plugins/job/ElementDescription/JobElementDescriptionCheckBox.java
+++
@@ -1,8 +0,0 @@
-package com.amcbridge.jenkins.plugins.job.ElementDescription;
-
-import org.w3c.dom.Document;
-
-public interface JobElementDescriptionCheckBox extends JobElementDescription {
-	public void uncheck(Document doc);
-	public void check(Document doc);
-}
--- branches/Dev/configurator/src/main/java/com/amcbridge/jenkins/plugins/vsc/VersionControlSystem.java
+++
@@ -1,7 +0,0 @@
-package com.amcbridge.jenkins.plugins.vsc;
-
-public interface VersionControlSystem
-{
-	public VersionControlSystemResult doCommit(String filePath,
-			String url, String login, String password);
-}
