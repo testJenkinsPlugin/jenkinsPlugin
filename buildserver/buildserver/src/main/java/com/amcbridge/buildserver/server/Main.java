b2dcfebf21b6bc86c6cdadd516dae2e295c3dab4
Oleksandr Kravets <okravets@amcbridge.com>
2016-08-16 17:36:12 +0300

    Review request #5085 com.amcbridge.buildserver.server.Main

--- buildserver/buildserver/src/main/java/com/amcbridge/buildserver/server/Main.java
+++ buildserver/buildserver/src/main/java/com/amcbridge/buildserver/server/Main.java
@@ -1,26 +1,18 @@
 package com.amcbridge.buildserver.server;
 
-
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class Main {
+    private static org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(BuildServer.class);
 
     public static void main(String[] args) throws Exception {
-        Logger.getLogger(BuildServer.class.getName()).log(Level.INFO, String.format("%n%s:\t%s%n", "STEP 1", "Start build server".toUpperCase()));
+        logger.info(String.format("%n%s:\t%s%n", "STEP 1", "Start build server".toUpperCase()));
         identifyEnvUserDir();
-
         BuildServer.Builder builder = BuildServer.newBuilder();
-        BuildServer server = builder
-                .setArgs(args)
-                .build();
-
-        Logger.getLogger(BuildServer.class.getName()).log(Level.INFO, String.format("%n%s:\t%s%n", "STEP 2", "Initialize build server".toUpperCase()));
+        BuildServer server = builder.setArgs(args).build();
+        logger.info(String.format("%n%s:\t%s%n", "STEP 2", "Initialize build server".toUpperCase()));
         server.init();
-
-        Logger.getLogger(BuildServer.class.getName()).log(Level.INFO, String.format("%n%s:\t%s%n", "STEP 3", "Execute build server".toUpperCase()));
+        logger.info(String.format("%n%s:\t%s%n", "STEP 3", "Execute build server".toUpperCase()));
         server.execute();
     }
 
@@ -29,7 +21,10 @@ public class Main {
         if (StringUtils.isNotEmpty(dir)) {
             System.setProperty("user.dir", System.getenv(BuildServer.Builder.KEY_ENV_INS_PATH));
         } else {
-            throw new BuildException(String.format("%s:\t%s%s%s%n%n", "ERROR", "You must explicitly set ", BuildServer.Builder.KEY_ENV_INS_PATH, " system variable to jar location"));
+            String message = String.format("%s:\t%s%s%s%n%n", "ERROR", "You must explicitly set ", BuildServer.Builder.KEY_ENV_INS_PATH, " system variable to jar location");
+            BuildException exception = new BuildException(message);
+            logger.error(message, exception);
+            throw exception;
         }
     }
 }
