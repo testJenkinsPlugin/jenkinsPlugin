82547da1ea8cc53b9031ff4cc9e148f452d1345e
Oleksandr Kravets <okravets@amcbridge.com>
2016-08-16 19:11:44 +0300

    Review request #5086 com.amcbridge.buildserver.server.ProjectVersion

--- buildserver/buildserver/src/main/java/com/amcbridge/buildserver/server/ProjectVersion.java
+++ buildserver/buildserver/src/main/java/com/amcbridge/buildserver/server/ProjectVersion.java
@@ -2,11 +2,7 @@ package com.amcbridge.buildserver.server;
 
 import com.amcbridge.jenkins.plugins.serialization.Project;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,6 +12,7 @@ public class ProjectVersion {
     private String minor;
     private String revision;
     private boolean versionFile;
+    private static org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(ProjectVersion.class);
 
     public void processVersions(Project project) {
         major = "";
@@ -23,256 +20,87 @@ public class ProjectVersion {
         if (project.getVersionFiles().isVersionFile() && project.getVersionFiles().getFiles().size() == 1) {
             initMajorMinorVersions(project.getVersionFiles().getFiles().get(0));
             versionFile = true;
-        }
-        else {
+        } else {
             initRevision(project.getRepository().getType());
         }
         printVersion();
     }
 
- /*   public String GetVersionString(String fileVer) {
-        String technologies = fileVer.substring(fileVer.indexOf('.') + 1);
-        String separator = ".";
-        if (technologies.compareToIgnoreCase("rc") == 0) {
-            separator = ",";
-        }
-
-        return major + separator + minor + separator + buildNumber + separator + revision;
-    }*/
-
     private void initRevision(String vcs) {
-        if (vcs.equalsIgnoreCase("git")) {
+        if ("git".equalsIgnoreCase(vcs)) {
             revision = System.getenv("GIT_COMMIT");
-            revision = revision.substring(revision.length()-12);
-            /*try {
-                System.setProperty("user.dir", System.getenv("WORKSPACE"));
-                Process p = Runtime.getRuntime().exec("git rev-list --count HEAD");
-                BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
-                revision = in.readLine();
-                in.close();
-            } catch (Exception ex) {
-                Logger.getLogger(ProjectVersion.class.getName()).log(Level.SEVERE, null, ex);
-            }*/
+            revision = revision.substring(revision.length() - 12);
         } else {
             revision = System.getenv("SVN_REVISION");
         }
     }
 
-    private boolean initMajorMinorVersions(String fileVer) {
+    private void initMajorMinorVersions(String fileVer) {
         String technologies = fileVer.substring(fileVer.indexOf('.') + 1);
         fileVer = System.getenv("WORKSPACE") + File.separator + fileVer;
+        File f = new File(fileVer);
+        char[] b = new char[(int) f.length()];
+        try (FileReader fr = new FileReader(f)) {
+            fr.read(b);
+            fr.close();
+        } catch (IOException ex) {
+            logger.error("Version file not found");
+        }
+        String tmp = new String(b);
         switch (technologies) {
             case "rc":
-                try {
-                    File f = new File(fileVer);
-                    char b[] = new char[(int) f.length()];
-                    FileReader fr = new FileReader(f);
-                    fr.read(b);
-                    fr.close();
-                    String tmp = new String(b);
-                    tmp = tmp.substring(tmp.indexOf("FILEVERSION"), tmp.indexOf("PRODUCTVERSION"));
-                    tmp = tmp.substring(tmp.indexOf('N') + 1);
-                    tmp = tmp.replaceAll(" ", "");
-                    major = tmp.substring(0, tmp.indexOf(','));
-                    tmp = tmp.substring(tmp.indexOf(',') + 1, tmp.length());
-                    minor = tmp.substring(0, tmp.indexOf(','));
-                } catch (Exception ex) {
-                    Logger.getLogger(ProjectVersion.class.getName()).log(Level.SEVERE, null, ex);
-                }
+                tmp = tmp.substring(tmp.indexOf("FILEVERSION"), tmp.indexOf("PRODUCTVERSION"));
+                tmp = tmp.substring(tmp.indexOf('N') + 1);
+                tmp = tmp.replaceAll(" ", "");
+                major = tmp.substring(0, tmp.indexOf(','));
+                tmp = tmp.substring(tmp.indexOf(',') + 1, tmp.length());
+                minor = tmp.substring(0, tmp.indexOf(','));
                 break;
             case "cs":
-                try {
-                    File f = new File(fileVer);
-                    char b[] = new char[(int) f.length()];
-                    FileReader fr = new FileReader(f);
-                    fr.read(b);
-                    fr.close();
-                    String tmp = new String(b);
-                    tmp = tmp.substring(tmp.indexOf("AssemblyFileVersion(\""));
-                    tmp = tmp.replaceAll("AssemblyFileVersion", "");
-                    tmp = tmp.replace('\"', ' ');
-                    tmp = tmp.replace(')', ' ');
-                    tmp = tmp.replace('(', ' ');
-                    tmp = tmp.replace(']', ' ');
-                    tmp = tmp.replaceAll(" ", "");
-                    major = tmp.substring(0, tmp.indexOf('.'));
-                    tmp = tmp.substring(tmp.indexOf('.') + 1, tmp.length());
-                    minor = tmp.substring(0, tmp.indexOf('.'));
-                } catch (Exception ex) {
-                    Logger.getLogger(ProjectVersion.class.getName()).log(Level.SEVERE, null, ex);
-                }
+                tmp = tmp.substring(tmp.indexOf("AssemblyFileVersion(\""));
+                tmp = tmp.replaceAll("AssemblyFileVersion", "");
+                tmp = tmp.replace('\"', ' ');
+                tmp = tmp.replace(')', ' ');
+                tmp = tmp.replace('(', ' ');
+                tmp = tmp.replace(']', ' ');
+                tmp = tmp.replaceAll(" ", "");
+                major = tmp.substring(0, tmp.indexOf('.'));
+                tmp = tmp.substring(tmp.indexOf('.') + 1, tmp.length());
+                minor = tmp.substring(0, tmp.indexOf('.'));
                 break;
             case "mf":
-                try {
-                    File f = new File(fileVer);
-                    char b[] = new char[(int) f.length()];
-                    FileReader fr = new FileReader(f);
-                    fr.read(b);
-                    fr.close();
-                    String tmp = new String(b);
-                    tmp = tmp.substring(tmp.indexOf("Manifest-Version:"), tmp.indexOf('\r'));
-                    tmp = tmp.replaceAll("Manifest-Version:", "");
-                    tmp = tmp.replaceAll(" ", "");
-                    major = tmp.substring(0, tmp.indexOf('.'));
-                    tmp = tmp.substring(tmp.indexOf('.') + 1, tmp.length());
-                    minor = tmp.substring(0);
-                } catch (Exception ex) {
-                    Logger.getLogger(ProjectVersion.class.getName()).log(Level.SEVERE, null, ex);
-                }
+                tmp = tmp.substring(tmp.indexOf("Manifest-Version:"), tmp.indexOf('\r'));
+                tmp = tmp.replaceAll("Manifest-Version:", "");
+                tmp = tmp.replaceAll(" ", "");
+                major = tmp.substring(0, tmp.indexOf('.'));
+                tmp = tmp.substring(tmp.indexOf('.') + 1, tmp.length());
+                minor = tmp.substring(0);
                 break;
             case "vb":
-                try {
-                    File f = new File(fileVer);
-                    char b[] = new char[(int) f.length()];
-                    FileReader fr = new FileReader(f);
-                    fr.read(b);
-                    fr.close();
-                    String tmp = new String(b);
-                    tmp = tmp.substring(tmp.indexOf("AssemblyFileVersion(\""));
-                    tmp = tmp.replaceAll("AssemblyFileVersion", "");
-                    tmp = tmp.replace('\"', ' ');
-                    tmp = tmp.replace(')', ' ');
-                    tmp = tmp.replace('(', ' ');
-                    tmp = tmp.replace('>', ' ');
-                    tmp = tmp.replaceAll(" ", "");
-                    major = tmp.substring(0, tmp.indexOf('.'));
-                    tmp = tmp.substring(tmp.indexOf('.') + 1, tmp.length());
-                    minor = tmp.substring(0, tmp.indexOf('.'));
-                } catch (Exception ex) {
-                    Logger.getLogger(ProjectVersion.class.getName()).log(Level.SEVERE, null, ex);
-                }
+                tmp = tmp.substring(tmp.indexOf("AssemblyFileVersion(\""));
+                tmp = tmp.replaceAll("AssemblyFileVersion", "");
+                tmp = tmp.replace('\"', ' ');
+                tmp = tmp.replace(')', ' ');
+                tmp = tmp.replace('(', ' ');
+                tmp = tmp.replace('>', ' ');
+                tmp = tmp.replaceAll(" ", "");
+                major = tmp.substring(0, tmp.indexOf('.'));
+                tmp = tmp.substring(tmp.indexOf('.') + 1, tmp.length());
+                minor = tmp.substring(0, tmp.indexOf('.'));
                 break;
             default:
-                System.out.println("Do not identify type of technologies!");
+                logger.error("Unknown version file extension");
                 break;
         }
-        return true;
     }
 
-   /* public boolean SetVersion(String fileVer, String strVer) {
-        String technologies = fileVer.substring(fileVer.indexOf('.') + 1);
-        fileVer = System.getenv("WORKSPACE") + File.separator + fileVer;
-        switch (technologies) {
-            case "rc":
-                try {
-                    File f = new File(fileVer);
-                    char b[] = new char[(int) f.length()];
-                    FileReader fr = new FileReader(f);
-                    fr.read(b);
-                    fr.close();
-                    String tmpFile = new String(b);
-                    String tmp = new String(b);
-                    tmp = tmp.substring(tmp.indexOf("FILEVERSION"), tmp.indexOf("PRODUCTVERSION"));
-                    tmp = tmp.substring(tmp.indexOf('N') + 1);
-                    tmp = tmp.replaceAll(" ", "");
-                    tmp = tmp.replaceAll("\r\n", "");
-                    tmp = tmp.trim();
-                    tmpFile = tmpFile.replaceAll(tmp, strVer);
-                    tmpFile = tmpFile.trim();
-                    FileWriter fw = new FileWriter(f);
-                    fw.write(tmpFile);
-                    fw.flush();
-                    fw.close();
-                } catch (Exception ex) {
-                    Logger.getLogger(ProjectVersion.class.getName()).log(Level.SEVERE, null, ex);
-                    return false;
-                }
-                break;
-            case "cs":
-                try {
-                    File f = new File(fileVer);
-                    char b[] = new char[(int) f.length()];
-                    FileReader fr = new FileReader(f);
-                    fr.read(b);
-                    String tmp = new String(b);
-                    String tmpFile = new String(b);
-                    tmp = tmp.substring(tmp.indexOf("AssemblyFileVersion(\""));
-                    tmp = tmp.replaceAll("AssemblyFileVersion", "");
-                    tmp = tmp.replace('\"', ' ');
-                    tmp = tmp.replace(')', ' ');
-                    tmp = tmp.replace('(', ' ');
-                    tmp = tmp.replace(']', ' ');
-                    tmp = tmp.replaceAll(" ", "");
-                    tmp = tmp.replaceAll("\r\n", "");
-                    tmp = tmp.trim();
-                    tmpFile = tmpFile.replaceAll(tmp, strVer);
-                    tmpFile = tmpFile.trim();
-                    FileWriter fw = new FileWriter(f);
-                    fw.write(tmpFile);
-                    fw.flush();
-                    fw.close();
-                } catch (Exception ex) {
-                    Logger.getLogger(ProjectVersion.class.getName()).log(Level.SEVERE, null, ex);
-                    return false;
-                }
-                break;
-            case "mf":
-                try {
-                    File f = new File(fileVer);
-                    char b[] = new char[(int) f.length()];
-                    FileReader fr = new FileReader(f);
-                    fr.read(b);
-                    String tmp = new String(b);
-                    String tmpFile = new String(b);
-                    tmp = tmp.substring(tmp.indexOf("Manifest-Version:"), tmp.indexOf('\r'));
-                    tmp = tmp.replaceAll("Manifest-Version:", "");
-                    tmp = tmp.replaceAll(" ", "");
-                    tmp = tmp.replaceAll("\r\n", "");
-                    tmp = tmp.trim();
-                    tmpFile = tmpFile.replaceAll(tmp, strVer);
-                    tmpFile = tmpFile.trim();
-                    FileWriter fw = new FileWriter(f);
-                    fw.write(tmpFile);
-                    fw.flush();
-                    fw.close();
-                } catch (Exception ex) {
-                    Logger.getLogger(ProjectVersion.class.getName()).log(Level.SEVERE, null, ex);
-                    return false;
-                }
-                break;
-            case "vb":
-                try {
-                    File f = new File(fileVer);
-                    char b[] = new char[(int) f.length()];
-                    FileReader fr = new FileReader(f);
-                    fr.read(b);
-                    String tmp = new String(b);
-                    String tmpFile = new String(b);
-                    tmp = tmp.substring(tmp.indexOf("AssemblyFileVersion(\""));
-                    tmp = tmp.replaceAll("AssemblyFileVersion", "");
-                    tmp = tmp.replace('\"', ' ');
-                    tmp = tmp.replace(')', ' ');
-                    tmp = tmp.replace('(', ' ');
-                    tmp = tmp.replace('>', ' ');
-                    tmp = tmp.replaceAll(" ", "");
-                    tmp = tmp.replaceAll("\r\n", "");
-                    tmp = tmp.trim();
-                    tmpFile = tmpFile.replaceAll(tmp, strVer);
-                    tmpFile = tmpFile.trim();
-                    FileWriter fw = new FileWriter(f);
-                    fw.write(tmpFile);
-                    fw.flush();
-                    fw.close();
-                } catch (Exception ex) {
-                    Logger.getLogger(ProjectVersion.class.getName()).log(Level.SEVERE, null, ex);
-                    return false;
-                }
-                break;
-            default:
-                System.out.println("Do not identify type of technologies!");
-                break;
-        }
-        return true;
-    }*/
-
     private void printVersion() {
-        StringBuilder version = new StringBuilder("");
+        StringBuilder version = new StringBuilder();
         if (versionFile) {
             version.append("version ").append(major).append(".").append(minor);
         } else {
             version.append("revision ").append(revision);
         }
-        System.out.println("[getting of version of the build started by hudson] " + version);
-
+        logger.info("[getting of version of the build started by hudson] " + version);
     }
 }
