63303396d64ce31d697393a2cde61d9607916c9f
dgorbunov <dgorbunov@amcbridge.com>
2016-11-09 14:14:14 +0200

    Review request #5220 com.amcbridge.jenkins.plugins.job.scm.JobGit - #8 Unused import 'import org.apache.log4j.Logger;' - apply formatting(remove redundant paragraphs)

--- trunk/configurator/src/main/java/com/amcbridge/jenkins/plugins/job/scm/JobGit.java
+++ trunk/configurator/src/main/java/com/amcbridge/jenkins/plugins/job/scm/JobGit.java
@@ -5,7 +5,6 @@ import com.amcbridge.jenkins.plugins.job.elementdescription.JobElementDescriptio
 import com.amcbridge.jenkins.plugins.job.JobManagerGenerator;
 import com.amcbridge.jenkins.plugins.job.JobSCM;
 import org.apache.commons.lang.StringUtils;
-import org.apache.log4j.Logger;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
@@ -22,7 +21,6 @@ public class JobGit implements JobElementDescription {
     private static final String XPATH_RELATIVE_GIT_CREDENTIALS_ID = "/userRemoteConfigs/hudson.plugins.git.UserRemoteConfig/credentialsId";
     private static final String XPATH_RELATIVE_GIT_BRANCH_NAME = "/branches/hudson.plugins.git.BranchSpec/name";
     private static final String XPATH_RELATIVE_GIT_LOCAL_DIRECTORY_PATH = "/extensions/hudson.plugins.git.extensions.impl.RelativeTargetDirectory/relativeTargetDir";
-
     private static final String TEMPLATE_PATH = "/plugins/build-configurator/job/scm/git.xml";
 
     @Override
@@ -37,18 +35,15 @@ public class JobGit implements JobElementDescription {
 
     @Override
     public String generateXML(BuildConfigurationModel config) throws XPathExpressionException, IOException, SAXException, ParserConfigurationException {
-
         if (config.getProjectToBuild() == null) {
             return StringUtils.EMPTY;
         }
-
         Document doc = JobManagerGenerator.loadTemplate(TEMPLATE_PATH);
         setProjectsConfigs(doc, config);
         return JobManagerGenerator.documentToXML(doc);
     }
 
     private void setProjectsConfigs(Document doc, BuildConfigurationModel config) throws XPathExpressionException {
-
         XPath xPath = XPathFactory.newInstance().newXPath();
         XPathExpression exp;
         // XPath has 1-based indexes. So start index is 1;
@@ -64,26 +59,17 @@ public class JobGit implements JobElementDescription {
             gitPlNode.getParentNode().appendChild(copyNode);
         }
 
-
         for (int projectIndex = 0; projectIndex < projectsQuantity; projectIndex++) {
-
             String scmPath = XPATH_CONCRETE_GIT_SCM + "[" + (projectIndex + 1) + "]";
-
             //branch name
             setSCMNodeValue(scmPath + XPATH_RELATIVE_GIT_BRANCH_NAME, config.getProjectToBuild().get(projectIndex).getBranchName(), doc);
-
             //url
             setSCMNodeValue(scmPath + XPATH_RELATIVE_GIT_URL, config.getProjectToBuild().get(projectIndex).getProjectUrl(), doc);
-
             //credentials
             setSCMNodeValue(scmPath + XPATH_RELATIVE_GIT_CREDENTIALS_ID, config.getProjectToBuild().get(projectIndex).getCredentials(), doc);
-
             //local folder
             setSCMNodeValue(scmPath + XPATH_RELATIVE_GIT_LOCAL_DIRECTORY_PATH, config.getProjectToBuild().get(projectIndex).getLocalDirectoryPath(), doc);
-
-
         }
-
     }
 
     private void setSCMNodeValue(String nodePath, String value, Document doc) throws XPathExpressionException {
@@ -100,5 +86,4 @@ public class JobGit implements JobElementDescription {
         JobSCM.removeSCM(doc);
         JobSCM.insertSCM(doc, generateXML(config));
     }
-
 }
